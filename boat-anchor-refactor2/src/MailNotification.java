e6d373a1bd4ddf618e0006e77d0fbe78db03309b
Ahmet Remzi Ata <ahmet.ata@trendyol.com>
2022-11-25 07:50:34 +0300

    Delete file

--- boat-anchor-refactor2/src/MailNotification.java
+++
@@ -1,5 +0,0 @@
-public class MailNotification {
-  public void SendMessage(String message) {
-    System.out.printf("Mail send. Message:%s%n", message);
-  }
-}
--- boat-anchor-refactor2/src/Main.java
+++
@@ -1,7 +0,0 @@
-public class Main {
-  public static void main(String[] args) {
-    System.out.println("Program started!");
-    MailNotification mailNotification = new MailNotification();
-    mailNotification.SendMessage("Lorem ipsum");
-  }
-}
