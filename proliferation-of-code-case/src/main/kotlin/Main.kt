81975299dfdcb098f4f2409372a466d676f741ac
Ahmet Remzi Ata <ahmet.ata@trendyol.com>
2022-11-25 06:59:13 +0300

    Add new codes

--- proliferation-of-code-case/src/main/kotlin/Main.kt
+++ proliferation-of-code-case/src/main/kotlin/Main.kt
@@ -1,10 +1,9 @@
 fun main(args: Array<String>) {
     println("Program started!")
 
-    //This object has no important purpose. Its purpose is only being a middleman
-    val proliferationObject = ProliferationObject(
-        notificationObject = NotificationObject(),
-        repositoryObject = RepositoryObject(),
-    )
-    proliferationObject.doSomething()
+    val repositoryObject = RepositoryObject()
+    val notificationObject = NotificationObject()
+
+    repositoryObject.addToDb()
+    notificationObject.notifyToCustomer()
 }
---
+++ proliferation-of-code-refactor/src/main/kotlin/NotificationObject.kt
@@ -0,0 +1,5 @@
+class NotificationObject() {
+    fun notifyToCustomer() {
+        println("Notify something!")
+    }
+}
