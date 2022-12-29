21ad4346d14bd2e8977c40d5f26eff1d705bca4b
Ahmet Remzi Ata <ahmet.ata@trendyol.com>
2022-12-29 06:49:57 +0300

    Change file

--- proliferation-of-code-case/src/main/kotlin/Main.kt
+++ proliferation-of-code-case/src/main/kotlin/Main.kt
@@ -1,9 +1,7 @@
 fun main(args: Array<String>) {
-    println("Program started!")
+    println("Hello World!")
 
-    val repositoryObject = RepositoryObject()
-    val notificationObject = NotificationObject()
-
-    repositoryObject.addToDb()
-    notificationObject.notifyToCustomer()
+    // Try adding program arguments via Run/Debug configuration.
+    // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
+    println("Program arguments: ${args.joinToString()}")
 }
--- proliferation-of-code-refactor/src/main/kotlin/Main.kt
+++ proliferation-of-code-refactor/src/main/kotlin/Main.kt
@@ -1,7 +1,9 @@
 fun main(args: Array<String>) {
-    println("Hello World!")
+    println("Program started!")
 
-    // Try adding program arguments via Run/Debug configuration.
-    // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
-    println("Program arguments: ${args.joinToString()}")
+    val repositoryObject = RepositoryObject()
+    val notificationObject = NotificationObject()
+
+    repositoryObject.addToDb()
+    notificationObject.notifyToCustomer()
 }
